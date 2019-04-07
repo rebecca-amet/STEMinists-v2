13ab2916a9e29db6edce217e2f70548d102af3e6
Lotje93 <47463063+Lotje93@users.noreply.github.com>
2019-04-07 12:41:40 +0100

    Updates contact page
    - Header & Footer updated
    - Included JS script

--- about_us/js/scripts.js
+++ about_us/js/scripts.js
@@ -1,3 +1,26 @@
-$("button").on("click", function() {
-  alert("Form is send")
+document.getElementById('status').innerHTML = "Sending...";
+formData = {
+'name'     : $('input[name=name]').val(),
+'surname'     : $('input[name=surname]').val(),
+'email'    : $('input[name=email]').val(),
+'need'  : $('input[name=subject]').val(),
+'message'  : $('textarea[name=message]').val()
+};
+
+
+$.ajax({
+url : "./php/contact.php",
+type: "POST",
+data : formData,
+success: function(data, textStatus, jqXHR)
+{
+
+$('#status').text(data.message);
+if (data.code) //If mail was sent successfully, reset the form.
+$('#contact-form').closest('form').find("input[type=text], textarea").val("");
+},
+error: function (jqXHR, textStatus, errorThrown)
+{
+$('#status').text(jqXHR);
+}
 });
